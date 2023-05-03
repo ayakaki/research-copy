a92890a2808f77ebd4a5864a466c36bd86f452d3
kakizawaayato <chibairodori25@gmail.com>
2023-05-03 09:34:50 +0900

    delete unnecessary file

--- research-app/pages/array/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NextPage } from 'next';
-import { useState } from 'react';
-
-const ArrayPage: NextPage = () => {
-  const [array, setArray] = useState([]);
-  return <></>;
-};
-
-export default ArrayPage;
